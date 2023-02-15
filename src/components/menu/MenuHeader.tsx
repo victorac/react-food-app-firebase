0d9d5e711fdebeacdbfce4bcc41187e81385cb4e
victorac <victorde.ac@gmail.com>
2023-02-15 09:34:48 -0300

    add CartButton to MenuHeader

--- src/components/menu/MenuHeader.tsx
+++ src/components/menu/MenuHeader.tsx
@@ -1,17 +1,11 @@
+import CartButton from "../cart/CartButton";
 import classes from "./MenuHeader.module.css";
-import cartIcon from "../../assets/material_cart.svg";
 
 const MenuHeader = () => {
   return (
     <div className={classes.header}>
       <span className={classes.title}>Food app</span>
-      <button className={classes.cardButton}>
-        <img src={cartIcon} className={classes.cartIcon} />
-        <span>Your cart</span>
-        <div className={classes.badge}>
-          {2}
-        </div>
-      </button>
+      <CartButton />
     </div>
   );
 };
