186a1e79293ef05f524d4f31491db7e044f2cbbc
victorac <victorde.ac@gmail.com>
2023-02-14 21:36:46 -0300

    add style to FoodItem

--- src/components/menu/FoodItemForm.tsx
+++ src/components/menu/FoodItemForm.tsx
@@ -2,8 +2,8 @@ import classes from  "./FoodItemForm.module.css";
 
 const FoodItemForm = () => {
     return <form className={classes.itemForm}>
-        <label className={classes.formLabel}>Amount</label>
-        <input className={classes.formInput} type="number" min={1} step={1}/>
+        <label className={classes.formLabel}>Amount:</label>
+        <input className={classes.formInput} type="number" min={1} step={1} max={10}/>
         <button className={classes.formAction} type="submit">Add</button>
     </form>
 }
