432e1bae76b5236ff73ea2ffc88b27e1eda8801a
victorac <victorde.ac@gmail.com>
2023-02-15 10:50:48 -0300

    start adding CartItem

--- src/components/cart/CartCard.tsx
+++ src/components/cart/CartCard.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Card from "../ui/Card";
 import classes from "./CartCard.module.css";
 
@@ -15,9 +17,15 @@ const CartCard: React.FC<Props> = ({ onOrder, onCancel }) => {
     onCancel();
   };
 
+  const context = useContext(CartContext);
+  const content = Object.keys(context.items).map(id => <li key={id}>{context.items[id].name}</li>)
+
   return (
     <Card className={classes.cartCard}>
       <span className={classes.title}>Your cart</span>
+      <ul>
+        {content}
+      </ul>
       <div className={classes.action}>
         <button onClick={cancelHandler}>Cancel</button>
         <button onClick={orderHandler}>Confirm</button>
