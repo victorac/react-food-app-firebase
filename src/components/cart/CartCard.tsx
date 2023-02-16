4978700896c23b46017247b1913b4302c5596073
victorac <victorde.ac@gmail.com>
2023-02-16 15:49:52 -0300

    finish basic CartCard

--- src/components/cart/CartCard.tsx
+++ src/components/cart/CartCard.tsx
@@ -19,17 +19,38 @@ const CartCard: React.FC<Props> = ({ onOrder, onCancel }) => {
   };
 
   const context = useContext(CartContext);
-  const content = Object.keys(context.items).map(id => <CartItem key={id} name={context.items[id].name} price={context.items[id].price} quantity={context.items[id].quantity} />)
+  const onUpdateItem = (id: string, price: string, value: number) => {
+    context.updateQuantity(id, price, value);
+  };
+
+  const isEmpty = Object.keys(context.items).length === 0;
+  let content: any = <p className={classes.textInfo}>The cart is empty</p>;
+  if (!isEmpty) {
+    content = Object.keys(context.items).map((id) => (
+      <CartItem
+        key={id}
+        id={id}
+        name={context.items[id].name}
+        price={context.items[id].price}
+        quantity={context.items[id].quantity}
+        onUpdateItem={onUpdateItem}
+      />
+    ));
+  }
 
   return (
     <Card className={classes.cartCard}>
       <span className={classes.title}>Your cart</span>
-      <ul className={classes.itemList}>
-        {content}
-      </ul>
+      <ul className={classes.itemList}>{content}</ul>
+      <div className={classes.total}>
+        <h3>Total</h3>
+        <span>${context.totalCost.toFixed(2)}</span>
+      </div>
       <div className={classes.action}>
         <button onClick={cancelHandler}>Cancel</button>
-        <button onClick={orderHandler}>Confirm</button>
+        <button onClick={orderHandler} disabled={isEmpty}>
+          Order
+        </button>
       </div>
     </Card>
   );
--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,12 +1,26 @@
 import classes from "./CartItem.module.css";
 
 interface Props {
+  id: string;
   name: string;
   price: string;
   quantity: number;
+  onUpdateItem: (id: string, price: string, quantity: number) => void;
 }
 
-const CartItem: React.FC<Props> = ({ name, price, quantity }) => {
+const CartItem: React.FC<Props> = ({
+  id,
+  name,
+  price,
+  quantity,
+  onUpdateItem,
+}) => {
+  const subtractHandler = () => {
+    onUpdateItem(id, price, -1);
+  };
+  const addHandler = () => {
+    onUpdateItem(id, price, 1);
+  };
   return (
     <li className={classes.item}>
       <div className={classes.info}>
@@ -14,10 +28,13 @@ const CartItem: React.FC<Props> = ({ name, price, quantity }) => {
         <div className={classes.value}>
           <span className={classes.price}>${price}</span>
           <div className={classes.badge}>
-          <span>x{quantity}</span>
+            <span>x{quantity}</span>
           </div>
-          
         </div>
+      </div>
+      <div className={classes.action}>
+        <button onClick={subtractHandler}>-</button>
+        <button onClick={addHandler}>+</button>
       </div>
     </li>
   );
